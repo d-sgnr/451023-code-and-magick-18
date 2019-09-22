4c6d04d101854661a51fe3dc504a8781e1674a79
David <dm.dsgnr@gmail.com>
2019-09-22 21:00:39 +0300

    Убирает магические числа и оптимизирует код

--- js/stat.js
+++ js/stat.js
@@ -4,24 +4,27 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
+var CLOUD_COLOR = '#fff';
+var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 var SHADOW_GAP = 10;
 
 var COLUMN_WIDTH = 40;
 var COLUMN_GAP = 50;
-var YOU_COLUMN_COLOR = 'rgba(255, 0, 0, 1)';
 var HISTOGRAM_TOP_GAP = 70;
 var HISTOGRAM_LEFT_GAP = 140;
 var MAX_COLUMN_HEIGHT = 150;
 
 var TEXT_HEIGHT = 10;
 var TEXT_GAP = 10;
+var TEXT_FONT = '16px PT Mono';
+var TEXT_COLOR = 'rgb(0, 0, 0)';
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
   for (var i = 1; i < arr.length; i++) {
     if (arr[i] > maxElement) {
@@ -31,33 +34,43 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, names, times) {
+var getColumnColor = function (names) {
+  for (var i = 0; i < names.length; i++) {
+    return names === 'Вы' ? 'red' : 'hsl(240, ' + Math.round(Math.random() * 100) + '%, 50%)';
+  } return null;
+};
 
-  var getColumnColor = function() {
-    if (names[i] === 'Вы') {
-      return 'red';
-    }
-    return 'hsl(240, ' + Math.round((Math.random() * 99 + 1)) + '%, 50%)';
-  };
+window.renderStatistics = function (ctx, names, times) {
 
-  var maxTime = getMaxElement(times);
+  renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, SHADOW_COLOR);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
 
-  renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  ctx.font = TEXT_FONT;
+  ctx.fillStyle = TEXT_COLOR;
 
-  ctx.font = '16px PT Mono';
-  ctx.fillStyle = 'rgb(0, 0, 0)';
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
 
+  var maxTime = getMaxElement(times);
+
   for (var i = 0; i < names.length; i++) {
 
-    ctx.fillText(Math.round(times[i]), HISTOGRAM_LEFT_GAP + (COLUMN_WIDTH + COLUMN_GAP) * [i], HISTOGRAM_TOP_GAP + MAX_COLUMN_HEIGHT + TEXT_HEIGHT - times[i] * MAX_COLUMN_HEIGHT / maxTime);
+    var columnX = HISTOGRAM_LEFT_GAP + (COLUMN_WIDTH + COLUMN_GAP) * i;
 
-    ctx.fillStyle = getColumnColor();
-    ctx.fillRect(HISTOGRAM_LEFT_GAP + (COLUMN_WIDTH + COLUMN_GAP) * [i], HISTOGRAM_TOP_GAP + TEXT_GAP + TEXT_HEIGHT + MAX_COLUMN_HEIGHT - times[i] * MAX_COLUMN_HEIGHT / maxTime, COLUMN_WIDTH, times[i] * MAX_COLUMN_HEIGHT / maxTime);
+    var columnHeight = times[i] * MAX_COLUMN_HEIGHT / maxTime;
 
-    ctx.fillStyle = 'black';
-    ctx.fillText(names[i], HISTOGRAM_LEFT_GAP + (COLUMN_WIDTH + COLUMN_GAP) * [i], HISTOGRAM_TOP_GAP + MAX_COLUMN_HEIGHT + 2 * TEXT_GAP + 2 * TEXT_HEIGHT);
-  };
+    var textNameY = HISTOGRAM_TOP_GAP + MAX_COLUMN_HEIGHT + 2 * TEXT_GAP + 2 * TEXT_HEIGHT;
+
+    var columnY = TEXT_GAP + HISTOGRAM_TOP_GAP + MAX_COLUMN_HEIGHT + TEXT_HEIGHT - columnHeight;
+
+    var textTimesY = HISTOGRAM_TOP_GAP + MAX_COLUMN_HEIGHT + TEXT_HEIGHT - columnHeight;
+
+    ctx.fillText(Math.round(times[i]), columnX, textTimesY);
+
+    ctx.fillStyle = getColumnColor(names[i]);
+    ctx.fillRect(columnX, columnY, COLUMN_WIDTH, columnHeight);
+
+    ctx.fillStyle = TEXT_COLOR;
+    ctx.fillText(names[i], columnX, textNameY);
+  }
 };
