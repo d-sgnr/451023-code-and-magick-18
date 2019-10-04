e362de0b63d5336fae6dd2c4d29d29445ddfb174
David <dm.dsgnr@gmail.com>
2019-10-04 10:40:12 +0300

    Открывает окно настройки и позволяет менять цвета персонажа

--- js/setup.js
+++ js/setup.js
@@ -5,15 +5,13 @@ var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', '�
 var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var WIZARDS_QTY = 4;
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-
-var showWizardSetup = function () {
-  var userDialog = document.querySelector('.setup');
-  userDialog.classList.remove('hidden');
-  var userSetup = document.querySelector('.setup-similar');
-  userSetup.classList.remove('hidden');
-};
 
 var getRandomItem = function (arr) {
   var randomIndex = Math.floor(Math.random() * arr.length);
@@ -52,8 +50,97 @@ var getWizards = function () {
 };
 
 var renderSetupScreen = function () {
-  showWizardSetup();
   getWizards();
 };
 
 renderSetupScreen();
+
+// Учебный проект: одеть Надежду
+
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+var inputName = document.querySelector('.setup-user-name');
+var setupForm = document.querySelector('.setup-wizard-form');
+var setupSubmit = document.querySelector('.setup-submit');
+var wizardCoat = document.querySelector('.setup-wizard .wizard-coat');
+var wizardEyes = document.querySelector('.setup-wizard .wizard-eyes');
+var wizardFireball = document.querySelector('.setup-fireball-wrap');
+var inputEyesColor = document.querySelector('input[name="eyes-color"]');
+var inputCoatColor = document.querySelector('input[name="coat-color"]');
+var inputFireballColor = document.querySelector('input[name="fireball-color"]');
+
+var changeElementColor = function (element, elementColors, input, property) {
+  element.addEventListener('click', function () {
+    var randomElementColor = getRandomItem(elementColors);
+    if (property === 'fill') {
+      element.style.fill = randomElementColor;
+    } else {
+      element.style.background = randomElementColor;
+    }
+    input.value = randomElementColor;
+  });
+};
+
+changeElementColor(wizardCoat, COAT_COLORS, inputCoatColor, 'fill');
+changeElementColor(wizardEyes, EYES_COLORS, inputEyesColor, 'fill');
+changeElementColor(wizardFireball, FIREBALL_COLORS, inputFireballColor, 'background');
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+var submitForm = function () {
+  setupForm.submit();
+};
+
+var onPopupEscPress = function (evt) {
+  if (document.activeElement === inputName) {
+    return null;
+  } else {
+    if (evt.keyCode === ESC_KEYCODE) {
+      return closePopup();
+    }
+  }
+  return null;
+};
+
+setupOpen.addEventListener('keydown', function () {
+  openPopup();
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('click', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+setupSubmit.addEventListener('click', function () {
+  submitForm();
+});
+
+setupSubmit.addEventListener('keydown', function (evt) {
+  if (evt.keydown === ENTER_KEYCODE) {
+    submitForm();
+  }
+});
